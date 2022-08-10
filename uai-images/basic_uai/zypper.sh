0f1cec20d88df2be393eebbf79ee83443a092fc6
Alan Mutschelknaus <81820210+alanm-hpe@users.noreply.github.com>
2022-08-10 13:34:52 -0500

    CASMSEC-344: Add support for docker secrets

---
+++ uai-images/basic_uai/zypper.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e +xv
+trap "rm -rf /root/.zypp" EXIT
+
+source /run/secrets/sles
+SLES_MIRROR="https://${ARTIFACTORY_USERNAME:-}${ARTIFACTORY_PASSWORD+:}${ARTIFACTORY_PASSWORD}@artifactory.algol60.net/artifactory/sles-mirror"
+SLES_VERSION=15-SP3
+ARCH=x86_64
+zypper --non-interactive rr --all
+zypper --non-interactive ar ${SLES_MIRROR}/Products/SLE-Module-Basesystem/${SLES_VERSION}/${ARCH}/product?auth=basic sles15sp3-Module-Basesystem-product
+zypper --non-interactive ar ${SLES_MIRROR}/Updates/SLE-Module-Basesystem/${SLES_VERSION}/${ARCH}/update?auth=basic sles15sp3-Module-Basesystem-update
+zypper update -y
+zypper install -y awk \
+                  curl \
+                  glibc-locale-base \
+                  gzip \
+                  iputils \
+                  jq \
+                  less \
+                  openssh \
+                  rsync \
+                  tar \
+                  vim \
+                  wget \
+                  which
+
+zypper addrepo --no-gpgcheck -f https://artifactory.algol60.net/artifactory/csm-rpms/hpe/stable/sle-15sp2 algol60
+zypper install -y --allow-unsigned-rpm cray-uai-util
+
+zypper clean -a && zypper --non-interactive rr --all && rm -f /etc/zypp/repos.d/*
