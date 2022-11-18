b70d215b8aa5097b27f09a31624154c0226c70ee
Michael Tupitsyn <mikhail.tupitsyn@hpe.com>
2022-11-18 12:24:05 -0800

    CASMINST-5612 Deprecate artifactory-algol60-readonly-file jenkins credential

--- uai-images/broker_uai/zypper-csm.sh
+++ uai-images/broker_uai/zypper-csm.sh
@@ -2,7 +2,8 @@
 set -e +xv
 trap "rm -rf /root/.zypp" EXIT
 
-source /run/secrets/sles
+ARTIFACTORY_USERNAME=$(test -f /run/secrets/ARTIFACTORY_READONLY_USER && cat /run/secrets/ARTIFACTORY_READONLY_USER)
+ARTIFACTORY_PASSWORD=$(test -f /run/secrets/ARTIFACTORY_READONLY_TOKEN && cat /run/secrets/ARTIFACTORY_READONLY_TOKEN)
 zypper --non-interactive rr --all
 zypper addrepo --no-gpgcheck -f https://${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD}@artifactory.algol60.net/artifactory/csm-rpms/hpe/stable/sle-15sp2 algol60
 zypper --non-interactive source-install cray-switchboard
